7bc6be96ec628719cce7a2f52543a44cc8198e01
u2rbs <111550177+u2rbs@users.noreply.github.com>
2022-08-28 22:05:50 -0400

    Create add_items.py

---
+++ dictionaries/add_items.py
@@ -0,0 +1,8 @@
+person = {"name": "John", "age": 20, "state": "TX"}
+# Using key-value assignment
+person["zip"] = 12345
+
+# Using Update() method
+person.update({"emp_id": 123, "ln": "unknown"})
+
+print(person)
